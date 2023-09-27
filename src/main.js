220f16d4ca7a384cf2eb69750178d152315ac85b
Jackson Whiting <jacksonwhiting00@gmail.com>
2023-09-26 21:40:10 -0400

    almost finished mobile view without javascript

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// const rangeSlider = document.querySelector("#range-slider")
+// rangeSlider.value = 60
+// rangeSlider.style.setProperty("--gradient-percent", `60%`)
+
+// rangeSlider.addEventListener("change", (e) => {
+// 	rangeSlider.style.setProperty("--gradient-percent", `${e.target.value}%`)
+// })
