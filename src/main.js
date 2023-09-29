c05ee8e275b911ad4948eab12eaa8107a39980cb
Jackson Whiting <jacksonwhiting00@gmail.com>
2023-09-29 14:21:20 -0400

    working with maps

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
-// const rangeSlider = document.querySelector("#range-slider")
-// rangeSlider.value = 60
-// rangeSlider.style.setProperty("--gradient-percent", `60%`)
+const rangeSlider = document.querySelector("#range-slider")
+rangeSlider.value = 10
 
-// rangeSlider.addEventListener("change", (e) => {
-// 	rangeSlider.style.setProperty("--gradient-percent", `${e.target.value}%`)
-// })
+let pricingStructure = new Map(
+   [
+      [
+         0, {
+            pageviews: "10k",
+            cost: "$8"
+      }],
+      [
+         5, {
+            pageviews: "50k",
+		   cost: "$12"
+         }
+      ]
+   ]
+)
+
+
+
+{
+	0: {
+		pageviews: "10k",
+		cost: "$8",
+	},
+	5: {
+		pageviews: "50k",
+		cost: "$12",
+	},
+	10: {
+		pageviews: "100k",
+		cost: "$16",
+	},
+	15: {
+		pageviews: "500k",
+		cost: "$24",
+	},
+	20: {
+		pageviews: "1M",
+		cost: "$36",
+	},
+}
