67969d5ac8f6446a017a9a20809c78c1db9afc1c
Jackson <jacksonwhiting00@gmail.com>
2023-10-02 10:54:14 -0400

    main.js work on maps

--- src/main.js
+++ src/main.js
@@ -1,43 +1,26 @@
 const rangeSlider = document.querySelector("#range-slider")
 rangeSlider.value = 10
 
-let pricingStructure = new Map(
-   [
-      [
-         0, {
-            pageviews: "10k",
-            cost: "$8"
-      }],
-      [
-         5, {
-            pageviews: "50k",
-		   cost: "$12"
-         }
-      ]
-   ]
-)
+let pricingStructure = new Map()
 
+pricingStructure.set(0, { pageviews: "10k", cost: "$8" })
+pricingStructure.set(5, { pageviews: "50k", cost: "$12" })
+pricingStructure.set(10, { pageviews: "100k", cost: "$16" })
+pricingStructure.set(15, { pageviews: "500k", cost: "$24" })
+pricingStructure.set(20, { pageviews: "1M", cost: "$36" })
 
+console.log(pricingStructure)
+let basePrice = pricingStructure.get(0)
+// console.log(basePrice.cost)
 
-{
-	0: {
-		pageviews: "10k",
-		cost: "$8",
-	},
-	5: {
-		pageviews: "50k",
-		cost: "$12",
-	},
-	10: {
-		pageviews: "100k",
-		cost: "$16",
-	},
-	15: {
-		pageviews: "500k",
-		cost: "$24",
-	},
-	20: {
-		pageviews: "1M",
-		cost: "$36",
-	},
-}
+rangeSlider.addEventListener("change", (e) => {
+	let sliderValue = e.target.value
+
+	for (const [key, value] of pricingStructure) {
+		if (sliderValue === key) {
+			console.log(value)
+		} else {
+			console.log(typeof key)
+		}
+	}
+})
