751731bb3e78b6248fe221621cf5065e79eff322
Jackson Whiting <jacksonwhiting00@gmail.com>
2023-10-03 14:05:18 -0400

    V1.0 complete

--- src/main.js
+++ src/main.js
@@ -1,26 +1,60 @@
 const rangeSlider = document.querySelector("#range-slider")
-rangeSlider.value = 10
+const annualPriceSwitchEl = document.querySelector("#annual-price-switch")
+const trafficPageviewsEl = document.querySelector("#traffic-pageviews")
+const trafficPriceEl = document.querySelector("#traffic-price")
 
-let pricingStructure = new Map()
+rangeSlider.value = 2
 
-pricingStructure.set(0, { pageviews: "10k", cost: "$8" })
-pricingStructure.set(5, { pageviews: "50k", cost: "$12" })
-pricingStructure.set(10, { pageviews: "100k", cost: "$16" })
-pricingStructure.set(15, { pageviews: "500k", cost: "$24" })
-pricingStructure.set(20, { pageviews: "1M", cost: "$36" })
+let pricingStructure = [
+	{ pageviews: "10k", cost: 8 },
 
-console.log(pricingStructure)
-let basePrice = pricingStructure.get(0)
-// console.log(basePrice.cost)
+	{ pageviews: "50k", cost: 12 },
+
+	{ pageviews: "100k", cost: 16 },
+
+	{ pageviews: "500k", cost: 24 },
+
+	{ pageviews: "1M", cost: 36 },
+]
+
+annualPriceSwitchEl.addEventListener("change", (e) => {
+	renderPrice()
+})
 
 rangeSlider.addEventListener("change", (e) => {
-	let sliderValue = e.target.value
-
-	for (const [key, value] of pricingStructure) {
-		if (sliderValue === key) {
-			console.log(value)
-		} else {
-			console.log(typeof key)
-		}
-	}
+	renderPrice()
 })
+
+let renderPrice = () => {
+	let sliderValue = Number(rangeSlider.value)
+
+	pricingStructure.forEach((package, i) => {
+		let price = package.cost
+
+		if (annualPriceSwitchEl.checked) {
+			price = price * 0.75
+		}
+
+		if (sliderValue === i) {
+			trafficPageviewsEl.textContent = package.pageviews
+			trafficPriceEl.textContent = `$${price}.00`
+			console.log(i, package.pageviews, package.cost)
+		}
+	})
+}
+// let pricingStructure = new Map()
+
+// pricingStructure.set(0, { pageviews: "10k", cost: "$8" })
+// pricingStructure.set(1, { pageviews: "50k", cost: "$12" })
+// pricingStructure.set(2, { pageviews: "100k", cost: "$16" })
+// pricingStructure.set(3, { pageviews: "500k", cost: "$24" })
+// pricingStructure.set(4, { pageviews: "1M", cost: "$36" })
+
+// rangeSlider.addEventListener("change", (e) => {
+// 	let sliderValue = Number(e.target.value)
+
+// 	pricingStructure.forEach((price, i) => {
+// 		console.log(i, price.pageviews)
+// 	})
+
+// })
